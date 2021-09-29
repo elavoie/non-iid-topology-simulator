a0657b95d55d830cea155a372dc490b223ae209d
Erick Lavoie <erick.lavoie@epfl.ch>
2021-09-29 22:19:56 +0200

    Fixed bug

--- tools/setup/topology/__init__.py
+++ tools/setup/topology/__init__.py
@@ -6,5 +6,8 @@ def load(rundir):
     edges = topology['edges']
     topology['edges'] = { int(rank):edges[rank] for rank in edges }
     topology['weights'] = torch.tensor(topology['weights'])
+    if 'neighbourhoods' in topology.keys():
+        ns = topology['neighbourhoods']
+        topology['neighbourhoods'] = { int(rank):ns[rank] for rank in ns }
     return topology
 
