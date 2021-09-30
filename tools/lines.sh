801e17ecd185e85ca09774d1f715ebc3d65a2324
Erick Lavoie <erick.lavoie@epfl.ch>
2021-09-30 14:24:57 +0200

    Added line counting script

---
+++ tools/lines.sh
@@ -0,0 +1,1 @@
+wc -l analyze/topology.py plot/convergence.py plot/convergence_helper.py setup/*.py setup/topology/*.py setup/model/*.py simulate/*.py simulate/algorithm/*.py setup/topology/d_cliques/*.py
