04d3317be3aacb29fa1627785c1ca5e04eb854c5
Erick Lavoie <erick.lavoie@epfl.ch>
2022-10-10 20:10:15 +0200

    Fixed bug

--- tools/simulate/logger.py
+++ tools/simulate/logger.py
@@ -111,15 +111,15 @@ class Logger:
 
             global_dir = os.path.join(self.rundir, "global")
             if not os.path.exists(global_dir):
-                os.mkdirs(global_dir)
+                os.mkdir(global_dir)
 
             global_model_dir = os.path.join(self.rundir, "global", "model")
             if not os.path.exists(global_model_dir):
-                os.mkdirs(global_model_dir)
+                os.mkdir(global_model_dir)
 
             global_meta_dir = os.path.join(self.rundir, "global", "meta")
             if not os.path.exists(global_meta_dir):
-                os.mkdirs(global_meta_dir)
+                os.mkdir(global_meta_dir)
 
             global_path = os.path.join(self.rundir, "global", "model", str(state["step"]))
             global_meta_path = os.path.join(self.rundir, "global", "meta", str(state["step"]))
